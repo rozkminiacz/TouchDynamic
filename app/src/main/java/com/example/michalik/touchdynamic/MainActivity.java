54012e25b67a9a9217fc5bd3ce5b2f82015d6f82
rozkmin <michalik@protonmail.ch>
2016-11-18 18:23:59 +0100

    layout changes

--- app/src/main/java/com/example/michalik/touchdynamic/MainActivity.java
+++ app/src/main/java/com/example/michalik/touchdynamic/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.michalik.touchdynamic;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -10,13 +12,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
 
 import com.example.michalik.touchdynamic.fragment.AboutFragment;
 import com.example.michalik.touchdynamic.fragment.MeasureFragment;
 import com.example.michalik.touchdynamic.fragment.SettingsFragment;
-
-import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -47,15 +46,13 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
         }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+//        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
@@ -75,30 +72,20 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-            fragmentManager.beginTransaction()
-                    .replace(R.id.content_main, new AboutFragment())
-                    .commit();
-
-        } else if (id == R.id.nav_measurement) {
-            fragmentManager.beginTransaction()
-                    .replace(R.id.content_main, new MeasureFragment())
-                    .commit();
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        switch (item.getItemId()){
+            case R.id.nav_about:
+                fragmentManager.beginTransaction()
+                        .replace(R.id.content_main, new AboutFragment())
+                        .commit();
+                break;
+            case R.id.nav_measurement:
+                fragmentManager.beginTransaction()
+                        .replace(R.id.content_main, new MeasureFragment())
+                        .commit();
+                break;
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
--- app/src/main/java/com/example/michalik/touchdynamic/fragment/AboutFragment.java
+++ app/src/main/java/com/example/michalik/touchdynamic/fragment/AboutFragment.java
@@ -3,28 +3,41 @@ package com.example.michalik.touchdynamic.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.michalik.touchdynamic.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AboutFragment extends Fragment {
 
+    @BindView(R.id.about_text)
+    TextView aboutText;
 
     public AboutFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_about, container, false);
+
+        ButterKnife.bind(this, view);
+        return view;
     }
 
+    private void setupViews(){
+//        aboutText.setText(Html.fromHtml());
+    }
 }
